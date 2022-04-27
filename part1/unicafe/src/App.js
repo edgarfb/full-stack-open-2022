3624c2ce89479b8f7482be506df34988925b7141
Edgar Britez <edgarfbritez@gmail.com>
2022-04-26 23:04:34 -0300

    exercise incafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
 
   return (
     <div>
@@ -16,6 +17,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {(100 * good) / all}</p>
     </div>
   );
 };
