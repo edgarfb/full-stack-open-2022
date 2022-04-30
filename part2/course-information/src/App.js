0db0d9b282051fa4d02955c742626a2ef2a01dc7
Edgar Britez <edgarfbritez@gmail.com>
2022-04-30 07:04:27 -0300

    part2 exercise 2.1 course-information step 9

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -42,32 +42,60 @@ const Course = ({ course }) => {
   );
 };
 
+const Courses = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
+};
+
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+        },
 
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-      },
-    ],
-  };
+        {
+          name: "State of a component",
+          exercises: 14,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return <Courses courses={courses} />;
 };
 
 export default App;
