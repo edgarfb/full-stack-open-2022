b56461cee4c4ecc25d6315877b37f7f5f9412fc6
Edgar Britez <edgarfbritez@gmail.com>
2022-05-15 17:14:11 -0300

    cors works

--- part2/the-phonebook/src/services/crud.js
+++ part2/the-phonebook/src/services/crud.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 // get all persons
 const getAll = () => {
-  return axios.get("http://localhost:3001/persons");
+  return axios.get("http://localhost:3001/api/persons");
 };
 
 const createPerson = (person) => {
-  return axios.post("http://localhost:3001/persons", person);
+  return axios.post("http://localhost:3001/api/persons", person);
 };
 
 const updatePersonNumber = (id, person) => {
   return axios
-    .put(`http://localhost:3001/persons/${id}`, person)
+    .put(`http://localhost:3001/api/persons/${id}`, person)
     .then((response) => response);
 };
 
 const deletePerson = (id) => {
-  return axios.delete(`http://localhost:3001/persons/${id}`);
+  return axios.delete(`http://localhost:3001/api/persons/${id}`);
 };
 
 export default { getAll, createPerson, updatePersonNumber, deletePerson };
